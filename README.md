96eefabacaea9e902a109462afdaf1a92d4f9b62
ydkulks <ydkulks2@gmail.com>
2021-02-16 21:11:18 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# reddit_scraping
+# Reddit_scraping
 Scraps the reddit website for top subreddit post
+
+Requirments:
+1)Install Python3
+2)Install Python package manager pip
+3)Install selenium package
+4)
---
+++ scrap.py
@@ -0,0 +1,40 @@
+#$ sudo apt-get install chromium-browser(if not installed:"chromedriver unexpectedly exited. Status code was: 127" error will occur)
+#https://stackoverflow.com/questions/46085270/selenium-common-exceptions-webdriverexception-message-chromedriver-executabl
+#Selenium docs: https://selenium-python.readthedocs.io/
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options 
+from selenium.webdriver.common.keys import Keys
+
+options = Options()
+options.add_argument("--headless")
+
+PATH =r'/mnt/e/code/py/chromedriver.exe'
+driver = webdriver.Chrome(chrome_options=options, executable_path=PATH)
+
+def main():
+    choice = input("\nEnter preferred subreddit:\nr/todayilearned\nr/learnpython\nr/YouShouldKnow [til/py/ysk]:")
+    if choice == 'til':
+        driver.get("https://reddit.com/r/todayilearned")
+        c = '//*[@id="SHORTCUT_FOCUSABLE_DIV"]/div[2]/div/div/div/div[2]/div[3]/div[1]/div[4]/div[2]/div'
+        content(c)
+    elif choice == 'py':
+        driver.get("https://reddit.com/r/learnpython/")
+        c = '//*[@id="SHORTCUT_FOCUSABLE_DIV"]/div[2]/div/div/div/div[2]/div[3]/div[1]/div[4]/div[3]/div'
+        content(c)
+    elif choice == 'ysk':
+        driver.get("https://reddit.com/r/YouShouldKnow/")
+        c = '//*[@id="SHORTCUT_FOCUSABLE_DIV"]/div[2]/div/div/div/div[2]/div[3]/div[1]/div[4]/div[5]/div'
+        content(c)
+
+def content(c):
+    subreddit = driver.find_element_by_xpath('//*[@id="SHORTCUT_FOCUSABLE_DIV"]/div[2]/div/div/div/div[2]/div[1]/div/div[1]/div/div[1]/h2')
+    print("\nName of the subreddit:",subreddit.text)
+    print("=======================================")
+    content = driver.find_element_by_xpath(c)
+    print(f"\nTop post of the day:\n{content.text}")
+
+
+if __name__=="__main__":
+    main()
+    driver.quit()
+
